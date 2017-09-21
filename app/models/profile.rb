83d112b59f1a89845814efe5b83fd8edb47a583f
Mohamed Elkarim <mohdawad2010@hotmail.com>
2017-09-21 14:14:07 -0400

    fixed some errors and styling issues

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,8 @@ class Profile < ApplicationRecord
   has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
   validates :username, presence: true, uniqueness: true, length: {minimum: 5, maximum: 20}
-  validates :firstname, presence: true
+  validates :firstname, presence: true, length: {minimum: 2, maximum: 20}
+  validates :lastname, presence: true, length: {minimum: 2, maximum: 20}
+  validates :age, length: {maximum: 2}
   belongs_to :user, optional: true
 end
